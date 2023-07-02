4e297b5cf797368ba93841500669ea6c79bd90ac
eightfeet <bajiao000000@163.com>
2023-07-02 15:24:39 +0800

    update

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -7,14 +7,6 @@ import { ModelType, Tags } from "~/types/models";
 import dayjs from "dayjs";
 const { t } = parse(window.location.hash.split("?")[1] || "");
 
-export const ndmap =
-  "eyIwIjoiQSIsIjEiOiJCIiwiMiI6IkMiLCIzIjoiRCIsIjQiOiJFIiwiNSI6IkYiLCI2IjoiRyIsIjciOiJIIiwiOCI6IkkiLCI5IjoiSiJ9";
-export const baseLicense = {
-  prodCode: "ART",
-  appVersion: "1.0",
-  osType: "IOS8",
-};
-
 export const px2vw = (px: number): string => {
   const result = px / 7.5;
   return `${result}vw`;
@@ -152,9 +144,39 @@ export const getImagePath = (item: ModelType) => {
   };
 };
 
+export const baseLicense = {
+  prodCode: "ART",
+  appVersion: "1.0",
+  osType: "IOS8",
+};
+
+const tamp = "TVRJek5EVTJONQ==";
+const ndmap = window.btoa(
+  `{${window
+    .atob(tamp)
+    .split("")
+    .map((el, i) => `"${i}":"${el}"`)
+    .join(",")}}`
+);
+
+export const encodeDate = (license: string, date: string) => {
+  const licenseArr = license.split("-");
+  const dateArr = dayjs(date)
+    .format("YYMMDD")
+    .split("")
+    .map((item) => JSON.parse(window.atob(ndmap))?.[item]);
+  return licenseArr
+    .map((item, index) => {
+      const el = `${dateArr[index]}${item}`;
+      return el;
+    })
+    .join("-");
+};
+
 export const decodeDate = (license: string) => {
   const licenseArr = license.split("-");
-  let date = "CA";
+  const md = JSON.parse(window.atob(ndmap));
+  let date = [md[2], md[0]].join("");
   const lice = licenseArr
     .map((item, index) => {
       date = `${date}${index && index % 2 === 0 ? "/" : ""}${item.slice(0, 1)}`;
@@ -168,24 +190,35 @@ export const decodeDate = (license: string) => {
   };
 };
 
-export const auth = async (name: string, license: string) => {
-  const { lice, date } = decodeDate(license);
-  const checkerData = JSON.parse(window.atob(ndmap));
-  const keysArr = Object.keys(checkerData);
-  const end = date
+export const dec = (license: string) => {
+  const { lice: l, date } = decodeDate(license);
+  const md = JSON.parse(window.atob(ndmap));
+  const inmap: { [key: string]: any } = {};
+  for (const key in md) {
+    if (Object.prototype.hasOwnProperty.call(md, key)) {
+      const element = md[key];
+      inmap[element] = key;
+    }
+  }
+
+  const e = date
     .split("")
     .map((item) => {
-      let el = item;
-      for (let index = 0; index < keysArr.length; index++) {
-        const key = keysArr[index];
-        const keyMap = checkerData[key];
-        if (el === keyMap) el = key;
+      if (inmap[item] !== undefined) {
+        return inmap[item];
       }
-      return el;
+      return item;
     })
     .join("");
+  return {
+    e,
+    l,
+  };
+};
 
-  if (dayjs(end).isBefore(dayjs())) {
+export const auth = async (name: string, license: string) => {
+  const { e, l } = dec(license);
+  if (dayjs(e).isBefore(dayjs())) {
     return Promise.reject(
       "%E6%BF%80%E6%B4%BB%E7%A0%81%E5%B7%B2%E8%BF%87%E6%9C%9F"
     );
@@ -195,8 +228,11 @@ export const auth = async (name: string, license: string) => {
     ...baseLicense,
     info: { name } as any,
   };
-  const validate = (await licenseKey.validateLicense(licData, lice)) as any;
+
+  const validate = (await licenseKey.validateLicense(licData, l)) as any;
   if (validate.errorCode === 0) {
+    console.log("33333");
+
     store.dispatch({
       type: "runtime/set",
       payload: {
